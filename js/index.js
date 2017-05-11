4ab340abb064e5a59e6acc6da627c8d257044b67
djugan2 <didguzzz@gmail.com>
2017-05-11 16:06:06 +0300

    1

---
+++ js/index.js
@@ -0,0 +1,6 @@
+$('document').ready(function() {
+	var options = { videoId: 'SkvSQ34gw-Q', start: 3 };
+	$('.wrapper').tubular(options);
+	// f-UGhWj1xww cool sepia hd
+	// 49SKbS7Xwf4 beautiful barn sepia
+});
