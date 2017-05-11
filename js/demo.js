1ba30210018376f08eea5a0d4f7e380e140fd041
djugan2 <didguzzz@gmail.com>
2017-05-11 21:33:00 +0300

    Landing

---
+++ js/demo.js
@@ -0,0 +1,24 @@
+/**
+ * Particleground demo
+ * @author Jonathan Nicol - @mrjnicol
+ */
+
+/*document.addEventListener('DOMContentLoaded', function () {
+  particleground(document.getElementById('particles'), {
+    dotColor: '#5cbdaa',
+    lineColor: '#5cbdaa'
+  });
+  var intro = document.getElementById('intro');
+  intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
+}, false);*/
+
+
+
+// jQuery plugin example:
+$(document).ready(function() {
+  $('#particles').particleground({
+    dotColor: '#4A6987',
+    lineColor: '#2C3E50'
+  });
+  
+});
